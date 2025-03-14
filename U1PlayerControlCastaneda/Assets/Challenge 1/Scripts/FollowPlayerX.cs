12704eb13c42c5d417d90c5781b132e1d6d3d98e
ethancastaneda1 <726750@student.dusd.net>
2025-03-14 09:54:10 -0700

    stupid plane
    work on challlenge 1 and figue why plane doosnet work

---
+++ U1PlayerControlCastaneda/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offsetMainCam = new Vector3(20,0,0);
+    private Vector3 camRotation = new Vector3(0,-90,0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        //offsetMainCam = transform.position - Plane.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // transform.SetPositionAndRotation(Plane.transform.position + offsetMainCam, Quaternion.Euler(camRotation));
+        //transform.position = player.transform.position + offset;
+    }
+}
--- U1PlayerControlCastaneda/Assets/Scripts/CameraSwitcher.cs
+++ U1PlayerControlCastaneda/Assets/Scripts/CameraSwitcher.cs
@@ -30,3 +30,49 @@ public class CameraSwitcher : MonoBehaviour
     }
 
 }
+//private variables
+//private float speed = 30f;
+//pr//ivate float turnSpeed = 45f;
+//private float horizontalInput;
+//private float forwardInput;
+
+
+// Start is called before the first frame update
+//void Start()
+//{
+
+//}
+
+// Update is called once per frame
+//void Update()
+//{
+//  horizontalInput = Input.GetAxis("Horizontal");
+//forwardInput = Input.GetAxis("Vertical");
+
+//Move the vehicle forward
+//    transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+//  transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+//}
+//}
+//public float speed = 5.0f;
+//public float rotationSpeed = 40f;
+//pu/blic float verticalInput;
+//public float horizontalInput;
+
+// Start is called before the first frame update
+//void Start()
+//{
+
+//}
+
+// Update is called once per frame
+//void FixedUpdate()
+//{
+    // get the user's vertical input
+  //  verticalInput = Input.GetAxis("Vertical");
+    //horizontalInput = Input.GetAxis("Horizontal");
+    // move the plane forward at a constant rate
+    //transform.Translate(Vector3.forward * speed * Time.deltaTime * horizontalInput);
+
+    // tilt the plane up/down based on up/down arrow keys
+//    transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput); 
